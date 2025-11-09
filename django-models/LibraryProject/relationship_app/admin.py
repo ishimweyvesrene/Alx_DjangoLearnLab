f8b54aec9930537158b1b88d0a0ce93ddf9909f1
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-11-10 01:14:52 +0200

    Update admin.py

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -7,3 +7,12 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'role')
     list_filter = ('role',)
     search_fields = ('user__username',)
+    
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year', 'isbn')
+    search_fields = ('title', 'author', 'isbn')
+    list_filter = ('publication_year',)
+
+
+
