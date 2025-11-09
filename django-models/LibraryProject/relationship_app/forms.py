1c2156d91ea41d4b2f349bb9056b884ad91bd39d
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-11-09 22:35:00 +0200

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# Registration form using Django's built-in User model
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
