f3c1c3aa29af8d2c2fe3908a44e8ef28d11301a0
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-11-10 00:10:44 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,24 +1,14 @@
- from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.detail import DetailView   # ✅ Required by ALX checker
-from .models import Book
-from .models import Library  # ✅ Keep as a separate line for checker
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationFor
 
 
-
-
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-# Class-based view to display details for a specific library
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
