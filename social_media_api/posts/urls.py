6409f177cb6929f9cd0ae66b24705e54bbf1879d
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-12-11 00:18:25 +0200

    Add LikeToggleAPIView to post URLs

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
+from .views import LikeToggleAPIView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -9,5 +10,5 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('feed/', FeedListAPIView.as_view(), name='feed'),
+     path('posts/<int:pk>/like/', LikeToggleAPIView.as_view(), name='post-like'),
 ]
