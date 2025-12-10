fb10f67f6bd5481495acfb1af7af7d0e9f335807
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-12-10 23:24:32 +0200

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,7 @@
 # accounts/urls.py
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-
+from .views import follow_user, unfollow_user, my_following, my_followers
 # Import views using lazy loading to avoid circular imports
 def get_register_view():
     from .views import RegisterAPIView
@@ -19,4 +19,8 @@ urlpatterns = [
     path('register/', get_register_view(), name='register'),
     path('login/', get_login_view(), name='login'),
     path('profile/', get_profile_view(), name='profile'),
+    path('follow/<int:user_id>/', follow_user, name='follow-user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
+    path('me/following/', my_following, name='my-following'),
+    path('me/followers/', my_followers, name='my-followers'),
 ]
