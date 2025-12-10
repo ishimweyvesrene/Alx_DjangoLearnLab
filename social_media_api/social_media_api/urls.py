9b04fa6f146ddb7c7ef72ddeb5011d3085d033ca
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-12-11 00:20:57 +0200

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
+    path('api/notifications/', include('notifications.urls')),
     # Add other app urls here
 ]
 
