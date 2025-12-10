9c5c6fbde12af3ef88f7b79454951d34e85cd68b
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-12-10 22:14:16 +0200

    Add 'posts' app to Django settings

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
